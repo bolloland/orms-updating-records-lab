53cb902405635facfedaf8daf7bd3741d455ee0b
Ryan Bollettino <ryan.bollettino@gmail.com>
2021-01-25 21:49:06 -0600

    commit tree 7de2d2110407108c89d6d565165281b15fb5ab1a

--- lib/student.rb
+++ lib/student.rb
@@ -1,7 +1,8 @@
 require_relative "../config/environment.rb"
-
+require 'pry'
 class Student
-    attr_accessor :name, :grade, :id
+    attr_accessor :name, :grade
+    attr_reader :id
     
 
     def initialize(id = nil, name, grade)
@@ -32,10 +33,10 @@ class Student
       if self.id
         self.update
       else
-        query =<<-TAB
-        INSERT INTO students(name, grade)
-        VALUES (?, ?)
-        TAB
+        query =<<-SQL
+          INSERT INTO students(name, grade)
+          VALUES (?, ?)
+        SQL
         DB[:conn].execute(query, self.name, self.grade)
         @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
       end
@@ -60,9 +61,12 @@ class Student
       # new_student.name = row[1]
       # new_student.grade = row[2]
       # new_student
-      id = row[0]
-      name = row[1]
-      grade = row[2]
+      # Mass Assignment
+      # row = [1,"pat", 12]
+      id, name, grade = row
+      # id = row[0]
+      # name = row[1]
+      # grade = row[2]
       self.new(id, name, grade)
     end
 
